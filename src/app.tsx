21ffa607e535be646f1bf1b6c178889975a37fff
lucky <298164449@qq.com>
2021-01-25 20:12:17 +0800

    upgrade dependent packages

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Button, message } from 'antd'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <div>Hello World!</div>
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    message.success('Hello World')
+                }}
+            >
+                Hello World!
+            </Button>
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
